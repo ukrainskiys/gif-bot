2fd75ebcaeb9fc3a59c6e270d5ce53416b4c2d2d
UkarainskiyS <https://github.com/UkrainskiyS>
2022-11-29 00:35:23 +0300

    fix packages

---
+++ build/build.sh
@@ -0,0 +1,11 @@
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+export GIPHY_TOKEN=$GIPHY_TOKEN
+export TELEGRAM_TOKEN=$TELEGRAM_TOKEN
+export YANDEX_OAUTH=$YANDEX_OAUTH
+
+docker compose up -d
+
+go build cmd/main/app.go
